17c74c0813d35d814c12b0e94b34ae9ad2034834
Patrick <hello@patrickpietens.com>
2020-05-28 16:03:01 +0200

    ...

--- src/sw.blade.php
+++ src/sw.blade.php
@@ -1,7 +1,7 @@
 importScripts('{{ themes_url("$theme/blokks-sw.js") }}');
 
 const prefix = '{{ $schedule->slug }}';
-const version = '3.0.0';
+const version = '4.0.0';
 const url = '{{ route('embeds.dedicated', ['schedules' => $schedule->slug ]) }}/';
 const updatedAt = '{{ $schedule->updated_at }}';
 
@@ -9,11 +9,10 @@ const files = [
 	url,
 	'{{ route('embeds.launcher', ['schedules' => $schedule->slug ]) }}',
 	'{{ themes_url("$theme/modern.js") }}',
-	'{{ themes_url("$theme/legacy.js") }}',
 	'{{ themes_url("$theme/columns.css") }}',
 	'{{ themes_url("$theme/rows.css") }}',
 	'{{ themes_url("$theme/list.css") }}',
-	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://fonts.googleapis.com/css2?family=Inter&family=Roboto+Slab:wght@500&display=swap',
 ];
 
 enableOffline({ 
